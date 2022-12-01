0d47e775eb2893914dd2d797fa65a82ffc4e3042
hanyusok <hanyusok@gmail.com>
2022-12-02 01:34:14 +0900

    app.vue 수정

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,17 @@ import AppFooter from "@/components/Footer.vue";
 </script>
 
 <template>
-  <div>
-    <navbar />
+  
+    <!-- <navbar /> -->
     <!-- <nav>
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
     </nav> -->
-  </div>
+  
   <router-view />
-  <div>
+  <!-- <div>
     <app-footer />>
-  </div>
+  </div> -->
 </template>
 
 <style scoped></style>
