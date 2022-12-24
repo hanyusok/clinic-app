07ed9b38ff2cdc2b254c681efd4d7dcef6797e82
hanyusok <hanyusok@gmail.com>
2022-12-25 08:38:59 +0900

    new call updated

--- src/components/NewCall.vue
+++ src/components/NewCall.vue
@@ -39,7 +39,7 @@
       <div class="col-md-6">
         <div class="card">
           <div class="card-header p-3 pb-0">
-            <h6 class="mb-1">사전 진료 안내</h6>
+            <h6 class="mb-1">사전 안내</h6>
             <p class="text-sm mb-0">Please follow this guide for a strong password:</p>
           </div>
           <div class="card-body p-3">
@@ -94,7 +94,7 @@
       <div class="col-md-6 pb-3">
         <div class="card">
           <div class="card-header pb-0 p-3">
-            <h6 class="mb-0">Basic Settings(기본설정)</h6>
+            <h6 class="mb-0">Basic Settings(사전 수납)</h6>
           </div>
           <div class="card-body p-3">
             <div class="form-group d-flex align-items-center justify-content-between">
@@ -154,9 +154,9 @@
         <div class="card">
           <div class="card-header p-3 pb-0">
             <div class="d-flex align-items-center">
-              <h6 class="mb-0">Two factor authentication</h6>
+              <h6 class="mb-0">진료비 수납</h6>
               <vsud-button color="dark" variant="gradient" size="sm" class="ms-auto mb-0"
-                >Enable</vsud-button
+                >본인부담 진료비 안내</vsud-button
               >
             </div>
           </div>
@@ -169,10 +169,10 @@
               <div class="card-body border-radius-lg bg-gradient-dark p-3">
                 <h6 class="mb-0 text-white">Questions about security?</h6>
                 <p class="text-white text-sm mb-4">
-                  Have a question, concern, or comment about security? Please contact us.
+                  {홍길동} Have a question, concern, or comment about security? Please contact us.
                 </p>
                 <vsud-button color="light" variant="gradient" class="mb-0"
-                  >Contact Us</vsud-button
+                  >이체하기</vsud-button
                 >
               </div>
             </div>
@@ -181,31 +181,10 @@
       </div>
     </div>
     <div class="row gx-4 mt-3">
-      <div class="col-md-6 pb-3">
-        <div class="card">
-          <div class="card-header p-3 pb-0">
-            <h6 class="mb-1">Change password</h6>
-            <p class="text-sm mb-0">
-              We will send you an email with the verification code.
-            </p>
-          </div>
-          <div class="card-body p-3">
-            <label class="form-label">Current password</label>
-            <input type="password" placeholder="Current Password" class="form-control" />
-            <label class="form-label">New password</label>
-            <input type="password" placeholder="New Password" class="form-control" />
-            <label class="form-label">Confirm new password</label>
-            <input type="password" placeholder="Confirm password" class="form-control" />
-            <vsud-button color="dark" variant="gradient" full-width class="w-100 mb-0"
-              >Update password</vsud-button
-            >
-          </div>
-        </div>
-      </div>
       <div class="col-md-6">
         <div class="card">
           <div class="card-header p-3 pb-0">
-            <h6 class="mb-1">Password requirements</h6>
+            <h6 class="mb-1">처방전 전송 안내</h6>
             <p class="text-sm mb-0">Please follow this guide for a strong password:</p>
           </div>
           <div class="card-body p-3">
@@ -226,6 +205,33 @@
           </div>
         </div>
       </div>
+      <div class="col-md-6 pb-3">
+        <div class="card">
+          <div class="card-header p-3 pb-0">
+            <h6 class="mb-1">처방전 수령하실 곳 입력해주셍</h6>
+            <p class="text-sm mb-0">
+              We will send you an email with the verification code.
+            </p>
+          </div>
+          <div class="card-body p-3">
+            <label class="form-label">약국 fax 번호</label>
+            <input type="password" placeholder="Current Password" class="form-control" />
+            <label class="form-label">비대면 신청자 email</label>
+            <input type="password" placeholder="New Password" class="form-control" />
+            <label class="form-label">기타 연락처</label>
+            <input type="password" placeholder="Confirm password" class="form-control" />
+            <vsud-button 
+            color="dark" 
+            variant="gradient" 
+            size="sm" 
+            type="button" 
+            class="btn-sm mb-0 mt-3"
+              >연락처 저장</vsud-button
+            >
+          </div>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
